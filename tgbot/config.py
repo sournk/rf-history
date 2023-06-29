5739c21bb7002da909032de258e84a44c31a3af8
Denis Kislitsyn <sournk@gmail.com>
2023-06-29 15:18:21 +0300

    LOGLEVEL to INFO

--- tgbot/config.py
+++ tgbot/config.py
@@ -6,4 +6,4 @@ TELEGRAM_API = os.getenv('RF_HISTORY_TELEGRAM_API')
 TRANS_TABLE_NAME = 'Trans'
 UPLOAD_FILES_PATH = 'files_received'
 SEND_FILES_PATH = 'files_sent'
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
